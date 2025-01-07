f77e21ba59b3dd696875e64b7d1296df9bb92649
Pratik <pratikrbokade05@gmailcom>
2025-01-08 00:55:21 +0530

    Added the Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is a basic  sentiment analysis project focusing on building a machine learning pipeline using DVC(Data Version Control)',
+    author='bpratik01',
+    license='',
+)
