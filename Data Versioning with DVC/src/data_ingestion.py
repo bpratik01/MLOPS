951e08b1114434c29fd37b29dfccea3f43eb0727
Pratik <pratikrbokade05@gmailcom>
2025-01-09 18:04:51 +0530

    added 2nd version of data

--- Data Versioning with DVC/src/data_ingestion.py
+++ Data Versioning with DVC/src/data_ingestion.py
@@ -11,8 +11,14 @@ df = df[df['Length of Membership'] > 1]
 df.drop(columns=['Avg. Session Length'], inplace = True)
 
 
+base_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 
-os.makedirs('data', exist_ok=True)
+os.makedirs(os.path.join(base_dir, 'data'), exist_ok=True)
 
-data_path = os.path.join('data', 'processed_data.csv')  
+data_path = os.path.join(base_dir, 'data', 'processed_data.csv')
+
+print("Processed data file will be stored at:", data_path)
+
+
 df.to_csv(data_path, index=False)
+
